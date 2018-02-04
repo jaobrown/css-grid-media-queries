9f6f800e94e843eba57ce98200b3f0d61312749b
Jared Brown <jaobrown@iupui.edu>
2018-02-03 21:59:38 -0500

    Added navigation/hamburger menu.
    Added navbar and hamburger menu with slide out menu.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,17 @@
 $('document').ready(function () {
     console.log("Page Loaded!");
+    toggleMenu();
 });
+
+function toggleMenu() {
+    $('#menu-btn').click(function () {
+        $('.navbar').animate({left: '0%'});
+        // console.log("works!");
+    });
+    $('#close-btn').click(function () {
+       $('.navbar').animate({left: '-60%'});
+        // console.log("close");
+    });
+}
+
 
